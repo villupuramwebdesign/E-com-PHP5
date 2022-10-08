4ec0986eaff3618c315ecd99d178a476a55d5a29
villupuramwebdesign <villupuramwebdesign1@gmail.com>
2022-10-08 23:17:54 +0100

    Update config.php

--- FrontEnd/config.php
+++ FrontEnd/config.php
@@ -4,7 +4,7 @@
     public static function getConnexion() {
       if (!isset(self::$instance)) {
 		try{
-        self::$instance = new PDO('mysql:host=localhost;dbname=projetweb', 'root', '');
+        self::$instance = new PDO('mysql:host=localhost;dbname=projetweb', 'root', 'root');
 		self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(Exception $e){
             die('Erreur: '.$e->getMessage());
